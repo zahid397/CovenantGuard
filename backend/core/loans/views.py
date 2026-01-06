4af1440d9eb8ed4377721e9976f51c37e6ed9f9f
zahid397 <zh05698@gmail.com>
2026-01-06 11:40:33 +0600

    Delete backend/core/loans/views.py

--- backend/core/loans/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from .models import Loan
-from .serializers import LoanSerializer
-
-class LoanViewSet(viewsets.ModelViewSet):
-    queryset = Loan.objects.all()
-    serializer_class = LoanSerializer
-  
