8a46e6cb6b99455f1efdf7c6c5639cd1f50e8855
zahid397 <zh05698@gmail.com>
2026-01-06 11:15:59 +0600

    Update wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,12 +1,6 @@
-"""
-WSGI config for CovenantGuard project.
-
-It exposes the WSGI callable as a module-level variable named `application`.
-"""
-
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 application = get_wsgi_application()
